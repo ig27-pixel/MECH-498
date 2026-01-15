ed3b90f5703410caad93eb3a50498caf41167a00
Isaiah Gonzalez <ig27@rice.edu>
2026-01-15 12:23:30 -0600

    Add __init__.py

---
+++ Lab-0/__init__.py
@@ -0,0 +1,12 @@
+from .investment_base import InvestmentBase
+from .investment_fund import InvestmentFund, AFund, BFund, CFund
+from .investment_student import StudentInvestment
+
+__all__ = [
+    "InvestmentBase",
+    "InvestmentFund",
+    "AFund",
+    "BFund",
+    "CFund",
+    "StudentInvestment",
+]
