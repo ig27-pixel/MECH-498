9be4114619425c77b728dcad632388257e3dcfbc
Isaiah Gonzalez <ig27@rice.edu>
2026-01-22 18:40:59 -0600

    Update lab1.py rotate function

--- lab1.py
+++ lab1.py
@@ -13,7 +13,21 @@ def rotate(P_B: np.ndarray) -> np.ndarray:
   Returns:
       np.ndarray: rotated array 
   """  
-  pass 
+  th_y = np.pi/2
+  th_z = np.pi/4
+
+  R_Y = np.array([[np.cos(th_y), 0, np.sin(th_y)],
+                    [0, 1, 0],
+                    [-np.sin(th_y), 0, np.cos(th_y)]])
+  
+  R_Z = np.array([[np.cos(th_z), -np.sin(th_z), 0],
+                    [np.sin(th_z), np.cos(th_z), 0],
+                    [0, 0, 1]])
+  
+  A_R_B = R_Y @ R_Z
+  B_R_A = A_R_B.T
+
+  return B_R_A @ P_B
 
 
 def euler_to_ht(angles: np.ndarray, pos: np.ndarray) ->np.ndarray:
