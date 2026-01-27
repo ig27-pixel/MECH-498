39b37406de3172b58992f969def35b841d7a4b77
Isaiah Gonzalez <ig27@rice.edu>
2026-01-27 15:59:10 -0600

    Update lab1.py rotate function

--- lab1.py
+++ lab1.py
@@ -70,7 +70,7 @@ def euler_to_ht(angles: np.ndarray, pos: np.ndarray) ->np.ndarray:
 
   A_T_B_inv = np.eye(4)
   A_T_B_inv[0:3, 0:3] = R.T
-  A_T_B_inv[:3, 3] = -R.T @ p.T
+  A_T_B_inv[:3, 3] = -R.T @ p
 
   return A_T_B, A_T_B_inv
 
