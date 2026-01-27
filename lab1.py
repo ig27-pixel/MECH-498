1f8e033c9724d77540f1e976733b1b0a5a393856
Isaiah Gonzalez <ig27@rice.edu>
2026-01-27 16:16:51 -0600

    Update lab1.py rotate function

--- lab1.py
+++ lab1.py
@@ -63,18 +63,18 @@ def euler_to_ht(angles: np.ndarray, pos: np.ndarray) ->np.ndarray:
                   [0, 0, 1]])
   
   # Z-Y_X rotation
-  A_R_B = R_Z @ R_Y @ R_X
+  R = R_Z @ R_Y @ R_X
 
-  A_T_B = np.eye(4)
-  A_T_B[0:3, 0:3] = A_R_B
-  A_T_B[:3, 3] = p
+  T = np.eye(4)
+  T[0:3, 0:3] = R
+  T[:3, 3] = p
 
-  A_T_B_inv = np.linalg.inv(A_T_B)
-  #A_T_B_inv = np.eye(4)
-  #A_T_B_inv[0:3, 0:3] = A_R_B.T
-  #A_T_B_inv[:3, 3] = -A_R_B.T @ p
+  T_inv = np.linalg.inv(T)
+  #T_inv = np.eye(4)
+  #T_inv[0:3, 0:3] = R.T
+  #T_inv[:3, 3] = -R.T @ p
 
-  return A_T_B, A_T_B_inv
+  return T, T_inv
 
 
 def rollr(angle: float) -> np.ndarray:
