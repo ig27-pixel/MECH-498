bbdb89c8361fc4701cde6abdcfe11e37cea6050a
Isaiah Gonzalez <ig27@rice.edu>
2026-02-10 17:27:34 -0600

    Update lab1.py phantom_fk function

--- lab1.py
+++ lab1.py
@@ -230,15 +230,16 @@ def phantom_fk(joint_angles: np.ndarray,
   len_3 = 170.0
 
   # DH Parameters 3 R joints
-  T_0_1 = screw_dh(0, 0, 0, joint_angles[0])
-  T_1_2 = screw_dh(len_2, 0, 0, joint_angles[1])
-  T_2_3 = screw_dh(0, 0, len_1 - len_3, joint_angles[2])
+  T_0_1 = screw_dh(0, -np.pi/2, 0, joint_angles[0])
+  T_1_2 = screw_dh(len_2, np.pi/2, len_1, joint_angles[1])
+  T_2_3 = screw_dh(0, 0, -len_3, joint_angles[2])
 
   # Wrist gimbal angles
   T_3_e = np.eye(4)
 
   # Gimabal rotations
-  T_e_g = rpytf(np.array([0,0,0,gimbal_angles[0],gimbal_angles[1],gimbal_angles[2]]))
+  T_e_g = np.eye(4)
+  T_e_g[0:3, 0:3] = rpyr(gimbal_angles)
 
   # Full Transformation
   phantom_T = np.array([T_0_1, T_1_2, T_2_3, T_3_e, T_e_g])
