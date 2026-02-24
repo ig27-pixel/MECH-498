42f6d65318dc0a5542491368e1691374e5551f77
Isaiah Gonzalez <ig27@rice.edu>
2026-02-23 18:37:57 -0600

    fix __init__.py file

--- Lab-2/__init__.py
+++
@@ -1,9 +0,0 @@
-from .drawing_helper import *
-from .lab1_utility import *
-from .lab1 import *
-
-__all__ = [
-    'general_utility',
-    'lab1_utility',
-    'lab1',
-]
---
+++ __init__.py
@@ -0,0 +1,13 @@
+from .drawing_helper import *
+from .fanuc import *
+from .general_utility import *
+from .robot_components import *
+from .test_script_student import *
+
+__all__ = [
+    'drawing_helper', 
+    'fanuc', 
+    'general_utility', 
+    'robot_components',
+    'test_script_student'
+]
