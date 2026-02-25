acad6053bf3facc94eb1e443eb0cbb78d4153f5f
Isaiah Gonzalez <ig27@rice.edu>
2026-02-24 18:24:29 -0600

    Update fanuc.py

--- fanuc.py
+++ fanuc.py
@@ -88,7 +88,7 @@ class Fanuc(object):
     self.a_4 = 0
     self.a_5 = 0
     self.l_1_z = 0
-    self.l_2_z = 900
+    self.l_2_z = -1000
     self.l_3_z = 0
     self.l_4_z = 1600
     self.l_5_z = 0
