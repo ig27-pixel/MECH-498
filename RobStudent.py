a78aea645eb17d079db53a5245ffe59faf445bcc
Isaiah Gonzalez <ig27@rice.edu>
2026-04-26 19:51:17 -0500

    Modify RobStudent.py for section 3 of Lab 4

--- RobStudent.py
+++ RobStudent.py
@@ -74,7 +74,7 @@ class RobStudent(RobSimulation):
     t_dwell1_end = 7.0
     t_arrive2 = 11.5
     t_dwell2_end = 13.0
-    t_arrive3 = 20.0
+    t_arrive3 = 25.0
 
     # Prefer the nominal home posture when multiple IK solutions exist.
     home_seed = np.array([0.0, np.radians(-20.0), np.radians(20.0)])
@@ -363,11 +363,11 @@ class RobStudent(RobSimulation):
         kp = np.array([280.0, 760.0, 320.0])
         kd = np.array([95.0, 260.0, 110.0])
       elif t < t3a:
-        kp = np.array([400.0, 1000.0, 450.0])
-        kd = np.array([150.0, 400.0, 175.0])
+        kp = np.array([260.0, 720.0, 300.0])
+        kd = np.array([110.0, 300.0, 130.0])
       else:
-        kp = np.array([200.0, 500.0, 220.0])
-        kd = np.array([120.0, 300.0, 135.0])
+        kp = np.array([280.0, 760.0, 320.0])
+        kd = np.array([95.0, 260.0, 110.0])
     else:
       t2a = t2e = -1.0
       t3a = -1.0
